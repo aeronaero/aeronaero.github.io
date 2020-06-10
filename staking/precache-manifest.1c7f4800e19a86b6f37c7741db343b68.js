c9ff6545ff9ed7111050dcfad674a1ed7db4f662
aeronaero <30949568+aeronaero@users.noreply.github.com>
2020-06-10 16:12:43 +0300

    Aeron Staking DApp Prototype

---
+++ staking/precache-manifest.1c7f4800e19a86b6f37c7741db343b68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d124e3366f8f2c678c6d9f025835cf84",
+    "url": "/staking/index.html"
+  },
+  {
+    "revision": "98312ecfc5eb4ae4cc42",
+    "url": "/staking/static/css/main.e5c91eaf.chunk.css"
+  },
+  {
+    "revision": "1304b4ffd663ccca2115",
+    "url": "/staking/static/js/2.1df2afcf.chunk.js"
+  },
+  {
+    "revision": "24c2375a177c6941bbdad571a75d58bf",
+    "url": "/staking/static/js/2.1df2afcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98312ecfc5eb4ae4cc42",
+    "url": "/staking/static/js/main.b41f1290.chunk.js"
+  },
+  {
+    "revision": "539a61fb6759818e6dd2",
+    "url": "/staking/static/js/runtime-main.355abd82.js"
+  },
+  {
+    "revision": "45c6ebc994a4ef717c33f216c2ca8f9c",
+    "url": "/staking/static/media/icon.45c6ebc9.png"
+  },
+  {
+    "revision": "ccc26baccbb4dc1de8bd6ca3478e3f56",
+    "url": "/staking/static/media/logo.ccc26bac.svg"
+  }
+]);
