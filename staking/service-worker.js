9635755953a43a8d1efe094e35dc4f6cdd2e7c6e
aeronaero <30949568+aeronaero@users.noreply.github.com>
2020-12-24 16:35:12 +0200

    Add files via upload

--- staking/service-worker.js
+++ staking/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/staking/precache-manifest.3cc303b80ebd6064ec5b0a7cca6b0c03.js"
+  "/staking/precache-manifest.10e96f2018a4ca6e62d29e9fd6050f3f.js"
 );
 
 self.addEventListener('message', (event) => {
